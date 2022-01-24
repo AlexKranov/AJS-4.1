9b0efd781eb34a07105e3764c2c021533d7da108
AlexKranov <alexkranov@mail.ru>
2022-01-25 00:10:22 +0300

    test

--- src/js/__tests__/health.test.js
+++ src/js/__tests__/health.test.js
@@ -1,9 +1,9 @@
 import getHealth from "../health";
 
 test.each([
-  [{ name: "magician", health: 13 }, "critycal"],
-  [{ name: "magician", health: 48 }, "wounded"],
-  [{ name: "magician", health: 92 }, "healthy"],
+  [{ name: "magician", health: 11 }, "critycal"],
+  [{ name: "magician", health: 50 }, "wounded"],
+  [{ name: "magician", health: 96 }, "healthy"],
 ])("%o", (status, expected) => {
   const result = getHealth(status);
   expect(result).toBe(expected);
